40773ddf7807b5386f2a3b5904282a3a312b255f
Ramzi <Reishiramzi@gmail.com>
2018-12-09 14:11:36 -0500

    c++ files

--- EndlessRunner/Source/EndlessRunner/SpawnVolume.cpp
+++ EndlessRunner/Source/EndlessRunner/SpawnVolume.cpp
@@ -1,77 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SpawnVolume.h"
-#include <Runtime/Engine/Classes/Components/BoxComponent.h>
-#include "Kismet/KismetMathLibrary.h"
-#include "Pickup.h"
-#include "Public/TimerManager.h"
 
-// Sets default values
-ASpawnVolume::ASpawnVolume()
+// Sets default values for this component's properties
+USpawnVolume::USpawnVolume()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
 
-	// create the spawn box
-	SpawnBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnBox"));
-	RootComponent = SpawnBox;
-
-	// set the spawn delay range
-	SpawnDelayLow = 1.0f;
-	SpawnDelayHigh = 4.5f;
+	// ...
 }
 
-// Called when the game starts or when spawned
-void ASpawnVolume::BeginPlay()
+
+// Called when the game starts
+void USpawnVolume::BeginPlay()
 {
 	Super::BeginPlay();
 
-	SpawnDelay = FMath::FRandRange(SpawnDelayLow, SpawnDelayHigh);
-	GetWorldTimerManager().SetTimer(SpawnTimer, this, &ASpawnVolume:: SpawnPickup, SpawnDelay, false);
+	// ...
 	
 }
 
+
 // Called every frame
-void ASpawnVolume::Tick(float DeltaTime)
+void USpawnVolume::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::Tick(DeltaTime);
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	// ...
 }
 
-FVector ASpawnVolume::GetRandomPointInVolume()
-{
-	FVector SpawnOrigin = SpawnBox->Bounds.Origin;
-	FVector SpawnExtent = SpawnBox->Bounds.BoxExtent;
-
-	return UKismetMathLibrary::RandomPointInBoundingBox(SpawnOrigin, SpawnExtent);
-}
-
-void ASpawnVolume::SpawnPickup()
-{
-	if (PickupToSpawn != NULL)
-	{
-		UWorld* const World = GetWorld();
-		if (World)
-		{
-			// set the spawn parameters
-			FActorSpawnParameters SpawnParams;
-			SpawnParams.Owner = this;
-			SpawnParams.Instigator = Instigator;
-
-			// Get a random location to spawn
-			FVector SpawnLocation = GetRandomPointInVolume();
-			
-			// Get a rotation for spawned pickup
-			FRotator SpawnRotation;
-			SpawnRotation.Yaw = FMath::FRand() * 360.0f;
-			SpawnRotation.Pitch = FMath::FRand() * 360.0f;
-			SpawnRotation.Roll = FMath::FRand() * 360.0f;
-
-			// spawn the pickup	
-			APickup* const SpawnedPickup = World->SpawnActor<APickup>(PickupToSpawn, SpawnLocation, SpawnRotation, SpawnParams);
-			
-			SpawnDelay = FMath::FRandRange(SpawnDelayLow, SpawnDelayHigh);
-			GetWorldTimerManager().SetTimer(SpawnTimer, this, &ASpawnVolume::SpawnPickup, SpawnDelay, false);
-		}
-	}
-}
--- EndlessRunner/Source/EndlessRunner/SpawnVolume.h
+++ EndlessRunner/Source/EndlessRunner/SpawnVolume.h
@@ -3,56 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "Components/ActorComponent.h"
 #include "SpawnVolume.generated.h"
 
-UCLASS()
-class ENDLESSRUNNER_API ASpawnVolume : public AActor
+
+UCLASS( ClassGroup=("SpawnVolume"), meta=(BlueprintSpawnableComponent) )
+class ENDLESSRUNNER_API USpawnVolume : public UActorComponent
 {
 	GENERATED_BODY()
-	
+
 public:	
-	// Sets default values for this actor's properties
-	ASpawnVolume();
+	// Sets default values for this component's properties
+	USpawnVolume();
 
 protected:
-	// Called when the game starts or when spawned
+	// Called when the game starts
 	virtual void BeginPlay() override;
-	// the pickup to spawn
-	UPROPERTY(EditAnywhere, Category = "Spawn Volume")
-	TSubclassOf<class APickup> PickupToSpawn;
-
-	// Spawn Timer times when to spawn pickup
-	FTimerHandle SpawnTimer;
-
-	// Spawn delay minimum
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn Volume")
-		float SpawnDelayLow;
-
-	// Spawn delay maximum
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn Volume")
-		float SpawnDelayHigh;
 
 public:	
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	// Returns Spawn Box volume
-	FORCEINLINE class UBoxComponent* GetSpawnBox() const { return SpawnBox; }
-
-	// returns a random point within the spawn volume
-	UFUNCTION(BlueprintPure, Category = "Spawn Volume")
-	FVector GetRandomPointInVolume();
-
-private:
-	// Spawning volume
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawn Volume", meta = (AllowPrivateAccess = "true"))
-		class UBoxComponent* SpawnBox;
-
-	// Spawn a pickup
-	void SpawnPickup();
-
-	// the current spawn delay
-	float SpawnDelay;
-
+		
 };
